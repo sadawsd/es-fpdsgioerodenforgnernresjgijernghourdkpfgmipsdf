30ef8959e78be2631e3bc5c4d8fb7e1dffee2525
sadawsd <40964844+sadawsd@users.noreply.github.com>
2018-07-08 23:33:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
+const prefix = 'a!'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -87,7 +87,26 @@ client.on('message', message => {
     }
 });
 
-
+client.on('message',function(message) {
+  if(message.content.startsWith (prefix +'server')) {
+    const millis = new Date().getTime() - message.guild.createdAt.getTime();
+    const noww = new Date();
+    dateFormat(noww, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
+    const createdAt = millis / 1000 / 60 / 60 / 24;
+    let embed = new Discord.RichEmbed()
+    .setThumbnail(message.author.avatarURL)
+    .addField(`${message.guild.name}`,`\`\`منذ ${createdAt.toFixed(0)} يوما \`\``)
+    .addField(':globe_with_meridians:** نوع السيرفر**',`[** __${message.guild.region}__ **]`,true)
+    .addField(':medal:** __الرتب__**',`[** __${message.guild.roles.size}__ **]`,true)
+    .addField(':red_circle:**__ عدد الاعضاء__**',`[** __${message.guild.memberCount}__ **]`,true)
+    .addField(':large_blue_circle:**__ عدد الاعضاء الاونلاين__**',`[** __${message.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
+    .addField(':pencil:**__ الرومات الكتابية__**',`[** __${message.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
+    .addField(':microphone:**__ رومات الصوت__**',`[** __${message.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
+    .addField(':crown:**__ الأونـر__**',`**${message.guild.owner}**`,true)
+    .addField(':id:**__ ايدي السيرفر__**',`**${message.guild.id}**`,true)
+    message.channel.send({embed:embed});
+  }
+});
 
 
 
